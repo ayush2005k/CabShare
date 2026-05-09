1c6e370527e943aeada7ed8150e028c2c3af31aa
Ayush Singh <ayushsingh100k@gmail.com>
2026-05-09 12:31:34 +0530

    CREATE CUSTOM USER MODEL

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('driver', 'Driver'),
+        ('rider', 'Rider'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
