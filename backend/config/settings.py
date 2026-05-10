a3fa2b217e2c20a5814afc463f5fd1a744a36a7d
Ayush Singh <ayushsingh100k@gmail.com>
2026-05-10 11:42:00 +0530

    backend auth complete: registration, login, and JWT token authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,3 +137,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
